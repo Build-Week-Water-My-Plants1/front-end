8cbc86973785457abec5fa4463a0e93c26c2c241
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-07-27 17:11:52 -0700

    update index.js

--- water-my-plants/src/index.js
+++ water-my-plants/src/index.js
@@ -6,6 +6,7 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -16,6 +17,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
