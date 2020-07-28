616dabc8b897999e6e0573507295fb6625a8d25e
Lindsay W <lwhite5314@gmail.com>
2020-07-27 20:03:28 -0400

    actions

--- water-my-plants/src/utils/PrivateRoute.js
+++ water-my-plants/src/utils/PrivateRoute.js
@@ -7,10 +7,8 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
       {...rest}
       render={props => {
         if (localStorage.getItem("token")) {
-          // if the token is in localStorage, render the given component
           return <Component {...props} />;
         } else {
-          // redirect to login
           console.log("redirecting");
           return <Redirect to="/login" />;
         }
