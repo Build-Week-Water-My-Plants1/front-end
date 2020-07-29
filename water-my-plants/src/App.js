20b876c84a009028de7445b9ad9951859910be53
Lindsay White <lwhite5314@gmail.com>
2020-07-29 14:18:46 -0400

    Revert "login"

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import './App.css';
 import PlantList from './Components/PlantList';
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import AddPlant from './Components/AddPlant';
 import Home from './Components/Home';
 import PrivateRoute from './utils/PrivateRoute';
-import { BrowserRouter as Switch } from 'react-router-dom';
+import {BrowserRouter as Switch } from 'react-router-dom';
 import Login from './Components/Login'
 
 
 function App() {
   return (
     <div className="App">
-
-      <Route exact path='/' component={Home} />
-      <Route path='/add' component={AddPlant} />
-
-
+    
       <Switch>
-        <PrivateRoute exact path='/plantlist' component={PlantList} />
-        <Route path='/login' component={Login} />
-        <Route component={Login} />
+      <Route exact path='/' component={Home} />
 
+      <PrivateRoute exact path ='/plantlist' component ={PlantList}/>
+      <Route path ='/login' component = {Login}/>
+      <Route path='/add' component={AddPlant} />
       </Switch>
     </div>
   );
 }
 export default App;
+      
+    
 
-
-
-
+  
 
