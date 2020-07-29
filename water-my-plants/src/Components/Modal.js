85baf5f383d24a1bdd9899f9df86784d0341f35f
Liz Shulman <corgipower@hotmail.com>
2020-07-29 15:34:59 -0400

    some cleanup

--- water-my-plants/src/Components/Modal.js
+++ water-my-plants/src/Components/Modal.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AddPlant from './AddPlant'
 import PlantForm from './PlantForm';
 
 const ModalExample = (props) => {
